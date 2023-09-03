1fcf4e5a23fbcd891349165eb6189a639e72499a
thdud <thgml1444@naver.com>
2023-09-03 23:52:51 +0900

    [ ADD-login/signupPage ] 로그인, 회원가입 기능을 구현하라 - next-auth를 사용함에 문제가 발생... DB에서 사용자 정보를 확인하여 로그인 하는 방법으로 변경 - signup페이지에서 userData를 MongoDB에 저장한 뒤, login페이지에서 입력데이터와 비교하는 방식으로 구현 - password는 아직 암호화를 하지 못함 -> 추후 예정 ( JWT - think ) - 로그인 한 유저의 데이터는 Cookies를 활용하여 받아오는 것으로 구현하였다

---
+++ pages/api/auth/login.js
@@ -0,0 +1,39 @@
+import { connectDB } from "@/app/utils/database/database";
+import { parse, serialize } from "cookie";
+
+export default async function handle(req, res) {
+  console.log(req.body);
+  let { userid, password } = req.body;
+
+  // 미션_유효성 검사 - 빈칸, email중복check, 길이제한 조건만들기
+  if (password == "" || userid == "") {
+    console.log("빈칸입니다");
+    return null;
+  }
+
+  if (req.method == "POST") {
+    let db = (await connectDB).db("Toonda");
+    let userInfo = await db.collection("user_card").findOne({ userid: userid });
+
+    if (!userInfo) {
+      console.log("해당 이메일은 없습니다");
+      return null;
+    }
+    if (userInfo.password !== password) {
+      console.log("아이디 혹은 비밀번호가 다릅니다");
+      return null;
+    }
+
+    // serialize 쿠키 문자열로 직렬화, 모든 페이지에서 사용, 만료일 설정
+    const serializedUserData = serialize("userData", JSON.stringify(userInfo), {
+      path: '/',
+      maxAge: 604800, // 1주일(초)
+    });
+    console.log("----쿠키----", serializedUserData);
+    
+    // 쿠키를 클라이언트에 보내기
+    // res.status(200).json("가입성공");
+    res.setHeader("Set-Cookie", [serializedUserData]);
+    res.redirect(302, "/");
+  }
+}
---
+++ pages/api/auth/singup.js
@@ -0,0 +1,27 @@
+import { connectDB } from "@/app/utils/database/database";
+// 암호화 라이브러리X
+import bcrypt from "bcrypt";
+
+export default async function handle(req, res) {
+  if (req.method == "POST") {
+    console.log("회원가입 req.body", req.body);
+
+    // 미션_ 유효성검사 빈칸, email중복check, 길이제한 조건만들기
+    // const hash = await bcrypt.hash(req.body.password, 10);
+    // req.body.password = hash;
+
+    const userData = {
+      'userid': req.body.userid,
+      'password': req.body.password,
+      'userProfileImage' : '',
+      'userIntro' : '',
+    }
+    console.log(userData)
+    let db = (await connectDB).db("Toonda");
+    await db.collection("user_card").insertOne(userData);
+    
+    return res.redirect(302, "/loginPage");
+    // res.status(200).json("가입성공");
+    // 미션_ 가입완료 메세지보내기
+  }
+}
