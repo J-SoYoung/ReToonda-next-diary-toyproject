30d150ab320b07a6b516f5b14944fcdeaf0079e1
thdud <thgml1444@naver.com>
2023-09-15 17:21:56 +0900

    [ ADD-carousel ] 캐러셀을 구현하라 - banner이미지의 파일명과 currntSlide의 숫자를 통일시켜 버튼을 클릭할 때마다 banner를 바뀌도록 구현하였다 - useEffect를 사용해 페이지가 렌더링 될 때 setInterval를 사용하여 autoplay를 구현하였고 - pause icon을 클릭하면 autoplay가 false가 되면서 useEffec의 return함수가 동작하여 clearInterval이 동작하도록 하였다.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./globals.css"
 import { connectDB } from "../public/utils/database/database";
 
 // 컴포넌트
 import Btn_PageUp from "./components/Btn_PageUp";
 import DiaryContentSection from "./components/DiaryContentSection";
+import ImageCarousel from "./components/ImageCarousel";
 
 export default async function Home() {
   const client = await connectDB;
@@ -12,10 +13,8 @@ export default async function Home() {
   const result = await db.collection("post").find().toArray();
 
   return (
-    <div className={styles.home}>
-      <section className={styles.carousel}>
-        <Image src={"/image/banner1.png"} alt="ex" width={500} height={200} />
-      </section>
+    <div className='home'>
+      <ImageCarousel/>
       <DiaryContentSection title="Today's DIARY" items={result} />
       <Btn_PageUp />
     </div>
