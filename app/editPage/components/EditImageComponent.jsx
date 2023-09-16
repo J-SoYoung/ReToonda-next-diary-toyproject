69677b23ccc8f52888f70e7b6594f83626c0c8f9
thdud <thgml1444@naver.com>
2023-09-16 15:28:56 +0900

    [ ADD-postEdit ] 수정기능 구현 - 서버컴포넌트로 mongodb에서 데이터를 받아 수정할 클라이언트 컴포넌트에 넘겨준다 - editInput컴포넌트에서는 사용자의 수정데이터를 받아서 서버로 edit요청을 한다 - 이때 이미지 컴포넌트는 따로 생성하여 이미지 미리보기 기능을 추가로 구현 - 또한 이미지 S3업로드도 util함수로 작성하여 컴포넌트에서 함수를 호출 및 imageFile을 인자로 넘겨 S3업로드를 수행한다 - util함수에서는 업로드 된 url을 반환하여 editInput컴포넌트에서 사용할 수 있도록 하였다 - post / edit의 기능이 비슷하므로 이후 두 컴포넌트를 합쳐서 구현해볼 생각

---
+++ app/editPage/components/EditImageComponent.jsx
@@ -0,0 +1,80 @@
+"use client";
+import React, { useContext, useState } from "react";
+import { PostDataContext } from "@/app/context/PostContext";
+import styles from "@/app/postPage/postPage.module.css";
+import Image from "next/image";
+
+export default function EditImageComponent({ imageDefault }) {
+  const { setPostState, imageFile } = useContext(PostDataContext);
+  const [previewImage, setPreviewImage] = useState("");
+
+  const handleImagePreview = async (e) => {
+    const file = e.target.files?.[0];
+    setPostState({
+      postError: null,
+      imageFile: file,
+    });
+
+    // 이미지 미리보기
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setPreviewImage(reader.result);
+      };
+    }
+  };
+
+  return (
+    <div className={styles.postImageBox}>
+      {/* input=file style변경 */}
+      <div className={styles.labelBox}>
+        {/* 실제 image업로드 input */}
+        <input
+          type="file"
+          name="image"
+          id="file"
+          accept="image/*"
+          onChange={handleImagePreview}
+        />
+        {!previewImage ? (
+          <>
+            {/* 기본 default 이미지 */}
+            <label htmlFor="file" className={styles.imageBasicLabel}>
+              <Image
+                src={imageDefault}
+                alt="default-image"
+                width={490}
+                height={400}
+              />
+            </label>
+          </>
+        ) : (
+          <div className={styles.imageEditLabelBox}>
+            <label htmlFor="file" className={styles.imageEditLabel}>
+              <p>이미지 변경</p>
+            </label>
+            <button
+              className={styles.imageEditLabel}
+              onClick={() => {
+                setPreviewImage("");
+              }}
+            >
+              이미지 삭제
+            </button>
+          </div>
+        )}
+      </div>
+      {/* Preview이미지 보여지는 부분 */}
+      {previewImage && (
+        <Image
+          className={styles.previewImg}
+          src={previewImage}
+          width={490}
+          height={400}
+          alt="preview-image"
+        />
+      )}
+    </div>
+  );
+}
