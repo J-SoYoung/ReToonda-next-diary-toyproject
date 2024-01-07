0e93f646b9b1734e147da8f5065c8227486aa100
thdud <thgml1444@naver.com>
2024-01-07 09:41:33 +0900

    test, s3대신 임시 이미지 추가

--- app/components/DiaryItem.jsx
+++ app/components/DiaryItem.jsx
@@ -1,32 +1,32 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from './pageComponent.module.css'
-import DiaryItemCommenNo from './DiaryItemCommenNo';
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./pageComponent.module.css";
+import DiaryItemCommenNo from "./DiaryItemCommenNo";
 
-
-export default function DiaryItem({item, type}) {
+export default function DiaryItem({ item, type }) {
   // const [ diaryType, setDiaryType ] = useState('grid')
-  if(type =='list'){
+  if (type == "list") {
     return (
       <Link href={`/detailPage/${item?._id}`} className={styles.listItemBox}>
-      <div className={styles.listContentItem}>
-        <Image
-          className={styles.itemImg}
-          alt="image"
-          src={item?.image}
-          width={110}
-          height={110}
+        <div className={styles.listContentItem}>
+          <Image
+            className={styles.itemImg}
+            alt="image"
+            // src={item?.image}
+            src="/public/image/free.jpg"
+            width={110}
+            height={110}
           />
-        <div className={styles.itemText}>
-          <div className={styles.itemTextBox}>
-            <p className={styles.date}>{item?.date}</p>
-            <DiaryItemCommenNo postid={item._id} type='list'/>
+          <div className={styles.itemText}>
+            <div className={styles.itemTextBox}>
+              <p className={styles.date}>{item?.date}</p>
+              <DiaryItemCommenNo postid={item._id} type="list" />
+            </div>
+            <p className={styles.itemTitle}>{item?.title}</p>
           </div>
-          <p className={styles.itemTitle}>{item?.title}</p>
         </div>
-      </div>
-    </Link>
-    )
+      </Link>
+    );
   }
   return (
     <Link href={`/detailPage/${item._id}`} className={styles.ItemBox}>
@@ -35,15 +35,15 @@ export default function DiaryItem({item, type}) {
           className={styles.itemImg}
           alt="image"
           src={item.image}
+          // src="/public/image/free.jpg"
           width={146}
           height={220}
-          />
+        />
         <div className={styles.itemText}>
-          <DiaryItemCommenNo postid={item._id} type='grid'/>
+          <DiaryItemCommenNo postid={item._id} type="grid" />
           <span className={styles.itemTitle}>{item.title}</span>
         </div>
       </div>
     </Link>
-  )
+  );
 }
-
